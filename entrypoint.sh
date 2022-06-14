b8288cefeed90b8c2656a2d292995aec0ed09407
Damian J ☁️ <damianjanikone@gmail.com>
2022-06-14 12:14:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-[ -z "$AWS_S3_BUCKET" ] && (echo "AWS_S3_BUCKET ($AWS_S3_BUCKET) is not set. Quitting."; exit 1)
+[ -z "$AWS_S3_BUCKET" ] && (echo "AWS_S3_BUCKET is not set. Quitting."; exit 1)
 [ -z "$AWS_ACCESS_KEY_ID" ] && (echo "AWS_ACCESS_KEY_ID is not set. Quitting."; exit 1)
 [ -z "$AWS_SECRET_ACCESS_KEY" ] && (echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."; exit 1)
 [ -z "$AWS_REGION" ] && AWS_REGION="us-east-1"
+
+[ -z "$S3_WEBSITE_INDEX" ] && S3_WEBSITE_INDEX="index.html" && (echo "S3_WEBSITE_INDEX set to default: $S3_WEBSITE_INDEX")
+[ -z "$S3_WEBSITE_ERROR" ] && S3_WEBSITE_ERROR="error.html" && (echo "S3_WEBSITE_ERROR set to default: $S3_WEBSITE_ERROR")
 
 PROFILE="s3-sync-action"
 
@@ -18,4 +21,7 @@ cat ~/.aws/credentials
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} --profile ${PROFILE} --no-progress
+aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} --profile ${PROFILE} --no-progress --acl public-read --follow-symlinks --delete
+aws s3 website s3://${AWS_S3_BUCKET}/${DEST_DIR} --index-document ${S3_WEBSITE_INDEX} --error-document ${S3_WEBSITE_ERROR}
+
+echo "http://$AWS_S3_BUCKET.s3-website-$AWS_REGION.amazonaws.com/"
