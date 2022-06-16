f0cc55ab799c03d96aa19d5ecf8ba5be09f7948d
Damian J ☁️ <damianjanikone@gmail.com>
2022-06-16 13:40:42 +0100

    Remove pre-sgined URL feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ set -e
 [ -z "$AWS_REGION" ] && AWS_REGION="us-east-1" && (echo "AWS_REGION set to default: $AWS_REGION")
 [ -z "$S3_WEBSITE_INDEX" ] && S3_WEBSITE_INDEX="index.html" && (echo "S3_WEBSITE_INDEX set to default: $S3_WEBSITE_INDEX")
 [ -z "$S3_WEBSITE_ERROR" ] && S3_WEBSITE_ERROR="error.html" && (echo "S3_WEBSITE_ERROR set to default: $S3_WEBSITE_ERROR")
-[ -z "$URL_EXPIRY" ] && URL_EXPIRY="300" && (echo "URL_EXPIRY set to default: $URL_EXPIRY")
 [ -z "$S3_WEBSITE_POLICY" ] && S3_WEBSITE_POLICY=$(cat <<-END
 {
     "Version": "2012-10-17",
@@ -23,7 +22,7 @@ set -e
                 "AWS": "*"
             },
             "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::$AWS_S3_BUCKET/${DEST_DIR:+/}static/*"
+            "Resource": "arn:aws:s3:::$AWS_S3_BUCKET/${DEST_DIR:+/}*"
         }
     ]
 }
@@ -43,19 +42,18 @@ aws s3api head-object --bucket $AWS_S3_BUCKET --key $S3_WEBSITE_INDEX >/dev/null
 # Sync files
 aws s3 sync ${SOURCE_DIR:-.} s3://$AWS_S3_BUCKET/$DEST_DIR --profile $AWS_IAM_PROFILE --no-progress
 
-# Create website
+# Create website if enabled and apply public policy
 if $WEBSITE
 then
     aws s3 website s3://$AWS_S3_BUCKET/$DEST_DIR --index-document $S3_WEBSITE_INDEX --error-document ${S3_WEBSITE_ERROR}
+    aws s3api put-bucket-policy --bucket $AWS_S3_BUCKET --policy "$S3_WEBSITE_POLICY"
+    
     echo "Webiste URL: http://$AWS_S3_BUCKET.s3-website-$AWS_REGION.amazonaws.com/"
+    echo "::set-output name=output_website_url::http://$AWS_S3_BUCKET.s3-website-$AWS_REGION.amazonaws.com/"
 fi
 
-# Update website policy
-aws s3api put-bucket-policy --bucket $AWS_S3_BUCKET --policy "$S3_WEBSITE_POLICY"
-
 # Generate pre-signed URLs for file
-PRESIGNED_URL=$(aws s3 presign s3://$AWS_S3_BUCKET${DEST_DIR:+/}/$S3_WEBSITE_INDEX  --expires-in $URL_EXPIRY)
-
-# Set Outputs for Github Actions
-echo "::set-output name=output_website_url::http://$AWS_S3_BUCKET.s3-website-$AWS_REGION.amazonaws.com/"
-echo "::set-output name=output_presigned_url::$PRESIGNED_URL"
+# TODO: Unmask AccountTokenId to display private URL
+# [ -z "$URL_EXPIRY" ] && URL_EXPIRY="300" && (echo "URL_EXPIRY set to default: $URL_EXPIRY")
+# PRESIGNED_URL=$(aws s3 presign s3://$AWS_S3_BUCKET${DEST_DIR:+/}/$S3_WEBSITE_INDEX  --expires-in $URL_EXPIRY)
+# echo "::set-output name=output_presigned_url::$PRESIGNED_URL"
